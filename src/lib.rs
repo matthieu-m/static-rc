e7ea43a64608bc1661895ff867412a626cd20552
Matthieu M <matthieum.147192@gmail.com>
2021-04-25 17:19:50 +0200

    Move lift to be stand-alone, improve it, hide it behind feature.
    * Changes:
    
    - Move lift to be a stand-alone function export.
    - Secure the lifetime of the callback.
    - Hide behind an experimental feature.
    
    * Motivation:
    
    `lift` is generic, and possibly useful beyond StaticRc, so it makes
    sense to decouple it.
    
    Yet at the same time it's also highly experimental, even more so than
    the rest of the crate, so it makes sense to hide it.

--- src/lib.rs
+++ src/lib.rs
@@ -79,3 +79,28 @@ mod rcref;
 #[cfg(feature = "alloc")]
 pub use self::rc::StaticRc;
 pub use self::rcref::StaticRcRef;
+
+/// Lifts `root` into the slot provided by `fun`; returns the previous value of the slot, if any.
+///
+/// This function is useful, for example, to "tie the knot" when appending 2 linked-lists: it is easy to splice the
+/// the head of the back linked-list at the back of the front linked-list, but then one has lost the head pointer
+/// and can no longer splice the tail of the front linked-list to it.
+///
+/// #   Experimental
+///
+/// This function is highly experimental, see the ongoing discussion at
+/// https://users.rust-lang.org/t/can-you-break-the-lift/58858.
+#[cfg(feature = "experimental-lift")]
+pub fn lift<F, R>(root: R, fun: F) -> R
+where
+    F: for<'a> FnOnce(&'a R) -> &'a mut R,
+{
+    let slot = fun(&root) as *mut R;
+
+    debug_assert_ne!(slot as *const _, &root as *const _);
+
+    //  Safety:
+    //  -   `root` is still alive, hence any reference linked to `root` is _also_ alive.
+    //  -   `slot` is necessarily different from `root`, being mutable.
+    core::mem::replace(unsafe { &mut *slot }, root)
+}
